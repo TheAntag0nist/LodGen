3a4f601bf81ce67ff949cd3c6fe8a2c082723496
TheAntag0nist <teorus2314@yandex.ru>
2023-02-22 12:43:32 +0300

    devlog: fix folder name

--- LodGenerator/src/lod_math.cpp
+++ LodGenerator/src/lod_math.cpp
@@ -1,12 +1,11 @@
 #include <lod_math.h>
 
-
 glm::vec3 get_face_normal(const glm::vec3 v1,const glm::vec3 v2,const glm::vec3 v3){
     glm::vec3 face_normal = {0,0,0};
 
     glm::vec3 ab = v1 - v2;
     glm::vec3 ac = v1 - v3;
-    
+
     face_normal = glm::cross(ab, ac);
     return glm::normalize(face_normal);
 }
